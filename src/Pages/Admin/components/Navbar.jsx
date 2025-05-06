76c1457c84224787448a9a855df256537687aaad
Pkr <pkrsri71@gmail.com>
2025-05-06 12:23:05 +0530

    NavBar changed

--- src/Pages/Admin/components/Navbar.jsx
+++ src/Pages/Admin/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import logo from "../../../assets/logo.png"; // Ice Factory logo (image + text)
-import settingsIcon from "../../../assets/settings.png";
+import logo from "../../../assets/mfpLogo.png"; // Ice Factory logo (image + text)
+import settingsIcon from "../../../assets/settings2.png";
 import adminprofileIcon from "../../../assets/profile.png";
 const Navbar = () => {
   return (
-    <div className="h-12 bg-blue-500/60 border-b-1 border-black flex items-center justify-between px-6 left-56 z-10">
-      <div className="flex ">
-        <img src={logo} alt="Ice Factory" className="h-10" />
-      </div>
+    <div className="h-15 bg-white border-b-1 border-black flex items-center justify-between px-6 left-56 z-10">
+      <div className="flex items-center space-x-1">
+  <img src={logo} alt="Ice Factory" className="h-14 w-14" />
+  <span className="text-2xl font-bold text-black">Muzaffarpur Ice</span>
+</div>
       <div className="flex items-center gap-4">
-        <button className="p-2 rounded-full bg-purple-100 hover:bg-purple-200">
-          <img src={settingsIcon} alt="Settings" className="w-4 h-4" />
+        <button className="p-2 rounded-full bg-white hover:bg-blue-400">
+          <img src={settingsIcon} alt="Settings" className="w-10 h-10" />
         </button>
-        <button className="p-2 rounded-full bg-purple-300 hover:bg-purple-400">
-          <img src={adminprofileIcon} alt="Profile" className="w-4 h-4" />
+        <button className="p-2 rounded-full bg-white hover:bg-blue-400">
+          <img src={adminprofileIcon} alt="Profile" className="w-8 h-8" />
         </button>
       </div>
     </div>
